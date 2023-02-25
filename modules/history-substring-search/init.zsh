d037ff0221c25b57157164e087f5fef30d455676
Topi 'top1' Tuulensuu <topi@topituulensuu.com>
2023-02-25 23:57:13 +0200

    i dont know what this does
    it complains about not finding the non-existent file, but if I make it
    not complain up-arrow stops working, so lets just fucking pipe the
    complain into void and forget about it, ok?

--- modules/history-substring-search/init.zsh
+++ modules/history-substring-search/init.zsh
@@ -11,7 +11,7 @@ pmodload 'editor'
 
 # Source module files.
 if (( ! $+functions[history-substring-search-up] )); then
-  source "${0:h}/external/zsh-history-substring-search.zsh" || return 1
+  source "${0:h}/external/zsh-history-substring-search.zsh" 2>/dev/null || return 1
 fi
 
 #
